77931d8d5b5fbe30440cd5f4a7aca9e32413dcdd
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-24 22:00:04 -0500

    Coding...

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
-import express = from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+require("dotenv").config();
 
+//Inits
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+
+//Middlewares
+
+//Static Files
+
+//Starting the server
+
+app.listen(app.get('port'), () => {
+    console.log(`Server On Port ${app.get('port')}`);
+});
+
+////////////////////////////////////
+
+/*
+const user = process.env.USERDB;
+    const pass = process.env.PASSWORDDB;
+    mongoose.connect(
+      `mongodb+srv://`+user+`:`+pass+
+      `@cluster0.pmfuk.mongodb.net/Data?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+      () => {
+        console.log("\n");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+        console.log("[INFO]: Loading Mondodb....");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+        console.log("[INFO]: Ready MongoDB ✅");
+        console.log(
+          `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+        );
+      }
+    );
+*/
